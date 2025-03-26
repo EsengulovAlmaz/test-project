5137010d4ecf9414d2efa580d8b9e070ed145ea9
Almaz Esengulov <almaz.esengulov.pi9.03.20@gmail.com>
2025-03-27 00:23:03 +0600

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
+const basename = '/test-project'
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>,
 )
